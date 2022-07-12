60cfe22e9e4a4cfa4b49e71280806fe9400648e6
CKougami <100983695+CKougami@users.noreply.github.com>
2022-07-13 07:52:10 +0900

    JavaScript読み込み

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'samples#index'
   get 'samples/index'
-  resources :methods, only: [:index]
+  resources :samples, only: [:index]
   get '2gether',      to: 'samples#2gether'
   get 'tharntype',    to: 'samples#tharntype'
   get 'changetolove', to: 'samples#changetolove'
