e71a722a617a2fb7a8a024886a1e3bbceee4e62f
CKougami <100983695+CKougami@users.noreply.github.com>
2022-07-16 16:28:40 +0900

    quizzes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -8,6 +11,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../changetolove")
+require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,3 +22,4 @@ require("../changetolove")
 
 import "bootstrap";
 import "../stylesheets/application.scss";
+
